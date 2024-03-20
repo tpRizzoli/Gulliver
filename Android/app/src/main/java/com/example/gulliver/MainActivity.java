4d44062534881efcab42634f77acaa21d286e996
Federic0C <federico.cribiu@itsrizzoli.it>
2024-03-20 17:51:18 +0100

    provato ad aggiungere il carosello (ma al momento l'ho lasciato da parte)
    fatto il collegamento del bottone "cerca itinerario" che manda al fragment "Tipologia"
    
    --- NB ----
    da modificare il fragment itinerari (dalla navbar sotto) che deve mostrare gli itinerari dell'utente loggato

--- Android/app/src/main/java/com/example/gulliver/MainActivity.java
+++ Android/app/src/main/java/com/example/gulliver/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.gulliver;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity
-        implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
     HomePageFragment homePageFragment = new HomePageFragment();
@@ -18,22 +17,18 @@ public class MainActivity extends AppCompatActivity
     ProfileShowFragment profileShowFragment = new ProfileShowFragment();
     ProfileShowFragment profileEditFragment = new ProfileShowFragment();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.homePage);
-
     }
 
     @Override
-    public boolean
-    onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item) {
         Fragment frag = null;
         int id = item.getItemId();
 
@@ -49,12 +44,38 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.btnModificaProfilo)
             frag = profileEditFragment;
 
+        // Cambia il fragment in base alla selezione dell'utente
+        changeFragment(frag);
+        return true;
+    }
 
-        assert frag != null;
+    // Metodo per cambiare il fragment
+    public void changeFragment(Fragment frag) {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.flFragment, frag)
+                .addToBackStack(null) // Aggiungi il fragment allo stack all'interno del back stack
                 .commit();
-        return true;
+    }
+
+    // Sovrascrivi il metodo onBackPressed per gestire correttamente la gesture "indietro"
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        int count = fragmentManager.getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+        } else {
+            // Se siamo nel fragment degli itinerari, torna al fragment dell'home page
+            Fragment homeFragment = fragmentManager.findFragmentByTag("homePage");
+            if (homeFragment != null && homeFragment.isVisible()) {
+                super.onBackPressed();
+            } else {
+                // Torna al fragment dell'home page
+                fragmentManager.popBackStack("homePage", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                bottomNavigationView.setSelectedItemId(R.id.main); // Seleziona l'elemento corrispondente nel BottomNavigationView
+            }
+        }
     }
 }

474701646cea061f3cccdc8790666b6b995386ed
Federic0C <federico.cribiu@itsrizzoli.it>
2024-04-09 13:00:39 +0200

    fix loginActivity + server.py

--- Android/app/src/main/java/com/example/gulliver/Activity/LoginActivity.java
+++ Android/app/src/main/java/com/example/gulliver/Activity/LoginActivity.java
@@ -63,14 +63,16 @@ public class LoginActivity extends AppCompatActivity {
                 public void onResponse(Call<User> call, Response<User> response) {
 
                     if (response.isSuccessful()) {
-                        //if(salvaCred.isChecked()) {
                             User utenteLoggato = response.body();
+                            if (utenteLoggato == null){
+                                Toast.makeText(LoginActivity.this, "Credenziali errate", Toast.LENGTH_SHORT).show();
+                            }else {
+                                savePreferencesData(utenteLoggato);
 
-                            savePreferencesData(utenteLoggato);
-                        //}
-                        Intent activity = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(activity);
-                        Toast.makeText(LoginActivity.this, "Accesso eseguito con successo!", Toast.LENGTH_SHORT).show();
+                                Intent activity = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(activity);
+                                Toast.makeText(LoginActivity.this, "Accesso eseguito con successo!", Toast.LENGTH_SHORT).show();
+                            }
                     } else {
                         Toast.makeText(LoginActivity.this, "Credenziali errate", Toast.LENGTH_SHORT).show();
                     }
